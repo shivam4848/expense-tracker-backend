3050624b221f92612ef93981be75c57e8bb2aa65
shivam3.sahu <shivam3.sahu@ril.com>
2025-08-06 15:42:34 +0530

    fixed

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
-	"expense-tracker-backend/models"
+	"expense-tracker-backend/database"
 	"expense-tracker-backend/routes"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
@@ -19,22 +15,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database")
-	}
-
-	models.DB = db
-	db.AutoMigrate(&models.User{})
+	database.ConnectDB()
 
 	r := gin.Default()
 	r.Static("/uploads", "./uploads")
